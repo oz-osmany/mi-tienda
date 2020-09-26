87614884ad209124961823bebb4255a076ac2896
Osmany <osmachado1981@gmail.com>
2020-09-26 11:59:42 -0500

    Iniciando la web

--- src/Apps.js
+++ src/Apps.js
@@ -1,23 +1,8 @@
 import "./scss/main.scss";
 import MobileMenu from "./scripts/MobileMenu";
-import "./images/banner.jpg";
-import "./images/logo-compras.png";
-import "./images/ropa-deportiva-mujer1.jpg";
-import "./images/tendenciass.jpg";
-import "./images/por-categorias.jpg";
-import "./images/mochilas1.jpg";
-import $ from "jquery";
-
-
+import "img";
 
 const mobileMenu=new MobileMenu();
-/*
-function cambio(mql){
-    alert(mql.matches);
-}
-var mql=window.matchMedia("screen and (max-width: 800px)");
-cambio(mql);
-mql.addListener(cambio);*/
 
 
 
--- src/img.js
+++ src/img.js
@@ -1,7 +1,9 @@
-class Img{
-    constructor() {
-        import(imgenes);
+    import "./images/banner.jpg";
+    import "./images/logo-compras.png";
+    import "./images/ropa-deportiva-mujer1.jpg";
+    import "./images/tendenciass.jpg";
+    import "./images/por-categorias.jpg";
+    import "./images/mochilas1.jpg";
+    import "./images/entrega-domicilio.jpg";
+    import "./images/pagos-tarjeta.jpg";
 
-    }
-}
-export default Img;
